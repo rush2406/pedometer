5e01188f321ea2dc8be8ff8fb8f6c2b6e5bb2e03
rush2406 <rushaligrandhe@gmail.com>
2017-12-04 16:30:28 +0400

    Pedometer

---
+++ app/src/main/java/com/example/rushali/myapplication/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.rushali.myapplication;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, StepListener {
+    private TextView TvSteps;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private int numSteps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Get an instance of the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+        TvSteps = (TextView) findViewById(R.id.tv_steps);
+        Button BtnStart = (Button) findViewById(R.id.btn_start);
+        Button BtnStop = (Button) findViewById(R.id.btn_stop);
+
+
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                numSteps = 0;
+                TvSteps.setText("0");
+                sensorManager.registerListener(MainActivity.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(MainActivity.this);
+                TvSteps.setText("");
+
+            }
+        });
+
+
+
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        TvSteps.setText(String.valueOf(numSteps));
+    }
+
+}
---
+++ app/src/main/java/com/example/rushali/myapplication/StepListener.java
@@ -0,0 +1,11 @@
+package com.example.rushali.myapplication;
+
+/**
+ * Created by rushali on 4/12/17.
+*/
+
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
